ea90e46a5960fa2de857daeb59828ddf0d44c4a4
nicolekjc <142810955+nicolekjc@users.noreply.github.com>
2023-11-01 14:12:47 +0000

    fixed

---
+++ scripts/cookie-banner.js
@@ -0,0 +1,8 @@
+// when #cookie-accept-button is clicked:
+$("#accept-cookies-button").click(function() {
+
+    console.log("button clicked");
+
+// add .hidden to #cookie-banner
+$("#cookie-banner").addClass("hidden");
+});
